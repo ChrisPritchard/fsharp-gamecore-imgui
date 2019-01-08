ba65a7a82250ba3dcde2abdc20479a8711fdb151
Christopher <chrispritchard.nz@gmail.com>
2019-01-08 19:34:15 +1300

    scaffolded project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # fsharp-gamecore-ui
+
 Simple UI elements for use with fsharp-gamecore in the development of games
---
+++ src/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace fsharp_gamecore_ui
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
