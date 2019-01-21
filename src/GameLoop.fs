58bd22b7774374ed2aed6ab4a324fd798c7d9524
Chris Pritchard <chris.pritchard@edpay.nz>
2019-01-22 12:27:00 +1300

    reworked to use a c# class lib containing the renderer

--- src/GameLoop.fs
+++ src/GameLoop.fs
@@ -1,6 +1,24 @@
 ﻿module GameCore.Imgui.GameLoop
 
-type ImguiGameLoop<'TModel> (config, updateModel, getView)
-    as this = 
-    inherit Game()
+open GameCore.GameLoop
+open imgui_renderer
 
+type ImguiGameLoop<'TModel> (config, updateModel, getView, getUI)
+    as this = 
+    inherit GameLoop<'TModel> (config, updateModel, getView)
+
+    let mutable imGuiRenderer = Unchecked.defaultof<ImGuiRenderer>
+
+    override __.Initialize() = 
+
+        imGuiRenderer = new ImGuiRenderer(this) |> ignore
+        imGuiRenderer.RebuildFontAtlas ()
+
+        base.Initialize ()
+
+    override __.Draw (gameTime) = 
+        base.Draw (gameTime)
+
+        imGuiRenderer.BeforeLayout(gameTime);
+        getUI this.currentModel
+        imGuiRenderer.AfterLayout();
