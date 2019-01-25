826dabea64ee47f79ad02a2f5f0105fd1331a3a8
Chris Pritchard <chris.pritchard@edpay.nz>
2019-01-25 14:23:29 +1300

    interim work hand writing own gui renderer

--- src/GameLoop.fs
+++ src/GameLoop.fs
@@ -1,26 +1,60 @@
 ﻿module GameCore.ImGui.GameLoop
 
 open GameCore.GameLoop
-open GameCore.ImGuiXnaRenderer
+open ImGuiNET
+open System.Runtime.InteropServices
+open Microsoft.FSharp.NativeInterop
+open Microsoft.Xna.Framework.Graphics
+open System
 
 type ImGuiGameLoop<'TModel, 'TUIModel> (config, updateModel, getView, startUIModel, getUI)
     as this = 
     inherit GameLoop<'TModel> (config, (fun runState model -> updateModel runState this.CurrentUIModel model), getView)
 
-    let mutable imGuiRenderer = Unchecked.defaultof<ImGuiRenderer>
     let mutable uiModel: 'TUIModel = startUIModel
 
+    let mutable loadedTextures = Map.empty
+    let mutable lastTextureId = 0;
+
     do
-        this.IsMouseVisible <- config.mouseVisible
+        ImGui.CreateContext ()
+        |> ImGui.SetCurrentContext
+    
+    //do
+        // setup input
+
+    let bindTexture texture = 
+        let id = new IntPtr (lastTextureId)
+        lastTextureId <- lastTextureId + 1
+        loadedTextures <- Map.add id texture loadedTextures
+        id
+    
+    let rebuildFontAtlas () =
+        let io = ImGui.GetIO ()
+        let (pixelData, width, height, bytesPerPixel) = io.Fonts.GetTexDataAsRGBA32 ()
+
+        let pixelData = NativePtr.toNativeInt pixelData
+        let pixels = Array.create<byte> (width * height * bytesPerPixel) 0uy
+        Marshal.Copy(pixelData, pixels, 0, pixels.Length)
+
+        let tex2d = new Texture2D(this.GraphicsDevice, width, height, false, SurfaceFormat.Color)
+        tex2d.SetData(pixels)
+        
+        let newId = bindTexture tex2d
+        io.Fonts.SetTexID newId
+        io.Fonts.ClearTexData ()
+
+    let updateInput io =
+        ()
+
+    let renderDrawData data =
+        ()
 
     member __.CurrentUIModel
         with get () = uiModel
     
     override __.Initialize() = 
-
-        imGuiRenderer <- new ImGuiRenderer(this)
-        imGuiRenderer.RebuildFontAtlas ()
-
+        rebuildFontAtlas ()
         base.Initialize ()
 
     override __.Draw (gameTime) = 
@@ -28,7 +62,15 @@ type ImGuiGameLoop<'TModel, 'TUIModel> (config, updateModel, getView, startUIMod
 
         match base.CurrentModel with
         | Some model -> 
-            imGuiRenderer.BeforeLayout(gameTime);
+            
+            let io = ImGui.GetIO()
+            io.DeltaTime <- float32 gameTime.ElapsedGameTime.TotalSeconds
+            updateInput io
+            ImGui.NewFrame ()
+
             uiModel <- getUI uiModel model
-            imGuiRenderer.AfterLayout();
+            
+            ImGui.Render ()
+            renderDrawData (ImGui.GetDrawData ())
+
         | _ -> ()
